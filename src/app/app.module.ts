2c4902cb4360a3910b7c7cc670e90944e36c4923
jasabira <jasbircoro.dev@gmail.com>
2022-08-27 01:49:34 -0300

    cambios y actualizaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { LineaComponent } from './components/linea/linea.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    NavComponent,
+    BannerComponent,
+    SobreMiComponent,
+    EducacionComponent,
+    SkillsComponent,
+    LineaComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
